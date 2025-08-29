03f1bb65a7a142eefd7adbe485380c07b7b87c66
Sh3noy <sunilshenoy@thetribe.team>
2025-08-29 17:45:12 +0530

    playstore build

--- flutter_ai_bot/android/app/build.gradle.kts
+++ flutter_ai_bot/android/app/build.gradle.kts
@@ -7,8 +7,9 @@ plugins {
 
 android {
     namespace = "com.example.flutter_ai_bot"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Override to satisfy plugin requirements
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
