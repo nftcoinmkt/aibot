c6f9cfa9f2d2a2c6d9b132b401d09fb4d7a1a46d
Sh3noy <sunilshenoy@thetribe.team>
2025-09-03 00:19:31 +0530

    error fixed

--- flutter_ai_bot/lib/views/main_navigation.dart
+++ flutter_ai_bot/lib/views/main_navigation.dart
@@ -124,7 +124,7 @@ class MainNavigationState extends State<MainNavigation> {
           color: Colors.white,
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withValues(alpha: 0.1),
+              color: Colors.black.withOpacity(0.1),
               blurRadius: 10,
               offset: const Offset(0, -2),
             ),
